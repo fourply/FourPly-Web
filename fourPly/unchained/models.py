c2b9b99d5a41d8c4fefb3ba1147a5cc1b4576317
Gurjit Singh <gurjit.singh@berkeley.edu>
2015-01-31 10:06:50 -0800

    models on models

--- fourPly/unchained/models.py
+++ fourPly/unchained/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+from django.db.models import Model
+
+
+class Bathroom(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    rating = models.DecimalField(decimal_places=2)
+    other_people = models.IntegerField()
+    lat = models.DecimalField()
+    lon = models.DecimalField()
+
+
+class Photo(models.Model):
+    uri = models.CharField(max_length=128)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    token = models.CharField()
+    check_ins = models.ManyToManyField(Bathroom)
+
