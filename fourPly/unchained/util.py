4c0b16962cb4c0a3c868304b789dbf04d696ef5e
Paul Moulton <pmoulton2@gmail.com>
2015-01-31 12:24:52 -0800

    Added is_nearby functionality and tests

--- fourPly/unchained/util.py
+++ fourPly/unchained/util.py
@@ -1,5 +1,6 @@
-from .models import UserProfile
-from django.contrib.auth import authenticate
+import math
+# from .models import UserProfile
+# from django.contrib.auth import authenticate
 
 
 def auth_user(request):
@@ -18,3 +19,45 @@ def auth_user(request):
         return profile
     else:
         return None
+
+
+
+def is_nearby(location1, location2, default_threshold=.4):
+    """
+    Return True or False if two latitude longitude tuples are relatively
+    close to each other. 
+    Input a default_threshold in kilometers.
+    
+    >>> a = (34.410977, -119.847042)
+    >>> freebirds = (34.413283, -119.855612)
+    >>> lagoon = (34.411583, -119.849765)
+    >>> is_nearby(a, freebirds)
+    False
+    >>> is_nearby(a, lagoon)
+    True
+    >>> is_nearby(lagoon, freebirds)
+    False
+    """
+    lat1, lon1 = location1
+    lat2, lon2 = location2
+
+    # Radius of the earth in km
+    radius = 6378.137
+
+    lat = deg_rad(lat1-lat2)
+    lon = deg_rad(lon1-lon2)
+
+    h = math.sin(lat/2)**2 + math.cos(deg_rad(lat1)) * math.cos(deg_rad(lat2)) * math.sin(lon/2)**2
+    i = 2 * math.atan2(math.sqrt(h), math.sqrt(1-h))
+    return i * radius < default_threshold
+
+def deg_rad(degrees):
+    """
+    Takes degrees returns radians
+    """
+    return degrees*math.pi/180
+
+
+
+
+
